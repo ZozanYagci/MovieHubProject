0fdbf005e4dce24140a404a3767cdf93caed9485
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-02-18 16:30:34 +0300

    first commit

---
+++ Infrastructure/MovieHub.Persistence/Context/MovieContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieHub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieHub.Persistence.Context
+{
+    public class MovieContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\MSSQLSERVER1;Initial Catalog=MovieHub;Integrated Security=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
