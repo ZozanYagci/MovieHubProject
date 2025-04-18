047bf52e29c4b706abcffe03a05eade94accddc1
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-04-18 14:58:45 +0300

    add template and customize homepage layout

---
+++ Clients/MvcWeb/MovieHub.WebApp/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieHub.WebApp.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult LayoutUI()
+        {
+            return View();
+        }
+    }
+}
---
+++ Clients/MvcWeb/MovieHub.WebApp/ViewComponents/UILayoutViewComponents/_UILayoutRegisterModalComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieHub.WebApp.ViewComponents.UILayoutViewComponents
+{
+    public class _UILayoutRegisterModalComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
